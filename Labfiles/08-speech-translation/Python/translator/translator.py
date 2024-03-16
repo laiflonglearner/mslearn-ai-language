c0d1d66ab93f1004a8e4397c99c844bbffd483aa
nadiarizkyhairunnisa <nadiarizkyh.dev@gmail.com>
2024-03-16 23:52:34 +0700

    add library, translation and speech config

--- Labfiles/08-speech-translation/Python/translator/translator.py
+++ Labfiles/08-speech-translation/Python/translator/translator.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import os
 
 # Import namespaces
-
+import azure.cognitiveservices.speech as speech_sdk
 
 def main():
     try:
@@ -16,15 +16,21 @@ def main():
         ai_region = os.getenv('SPEECH_REGION')
 
         # Configure translation
-
+        translation_config = speech_sdk.translation.SpeechTranslationConfig(ai_key, ai_region) #to translate spoken input
+        translation_config.speech_recognition_language = 'en-US'
+        translation_config.add_target_language('fr')
+        translation_config.add_target_language('es')
+        translation_config.add_target_language('hi')
+        translation_config.add_target_language('de')
+        print('Ready to translate from',translation_config.speech_recognition_language)
 
         # Configure speech
-
-
+        speech_config = speech_sdk.SpeechConfig(ai_key, ai_region) #to synthesize translations into speech
+        
         # Get user input
         targetLanguage = ''
         while targetLanguage != 'quit':
-            targetLanguage = input('\nEnter a target language\n fr = French\n es = Spanish\n hi = Hindi\n Enter anything else to stop\n').lower()
+            targetLanguage = input('\nEnter a target language\n fr = French\n es = Spanish\n hi = Hindi\n de = German \n Enter anything else to stop\n').lower()
             if targetLanguage in translation_config.target_languages:
                 Translate(targetLanguage)
             else:
@@ -33,7 +39,7 @@ def main():
 
     except Exception as ex:
         print(ex)
-
+ 
 def Translate(targetLanguage):
     translation = ''
 
